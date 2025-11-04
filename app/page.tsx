88fbd0da9a399af8fd2c70b962bda85317198091
Emmanuel Sanchez <emmsanchezaranda@hotmail.com>
2025-11-04 16:06:37 -0600

    Añadiendo autenticación con Email y Password

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,76 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { createClient } from "@/lib/supabase/client";
+import { useRouter } from "next/navigation";
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 const redirectUrl = `${baseUrl}/auth/callback?next=/dashboard`;
 
 export default function Home() {
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+  const [showPassword, setShowPassword] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
 
   const handleLoginWithAzure = async () => {
     const supabase = createClient();
     setError(null);
-
     try {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: "azure",
         options: {
           redirectTo: redirectUrl,
           scopes: "email offline_access",
-          queryParams: {
-            prompt: "select_account",
-          },
+          queryParams: { prompt: "select_account" },
         },
       });
       if (error) throw error;
     } catch (error: unknown) {
-      setError(error instanceof Error ? error.message : "An error occurred");
+      setError(
+        error instanceof Error
+          ? error.message
+          : "Error al iniciar sesión con Azure"
+      );
+    }
+  };
+
+  const handleLoginWithEmail = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null);
+    setIsLoading(true);
+
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+
+    const supabase = createClient();
+
+    try {
+      const { error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) {
+        setError("Correo o contraseña incorrectos");
+        console.error(error);
+        return;
+      }
+
+      // Si llega aquí, el login fue exitoso
+      router.push("/dashboard");
+    } catch (err) {
+      setError("Error al procesar la solicitud");
+      console.error(err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <div className="relative w-full h-dvh flex flex-row text-black">
+      {/* Imagen izquierda */}
       <div className="max-md:hidden w-[900px] min-h-screen relative">
         <Image
           src={"/assets/img/login_wallpaper.jpg"}
@@ -70,38 +108,39 @@ export default function Home() {
             Iniciar Sesión
           </h2>
 
-          {/* Log in with microsoft */}
+          {/* Login con Microsoft */}
           <button
             onClick={handleLoginWithAzure}
-            className="flex justify-center items-center gap-4 py-3 w-full rounded-md bg-[#E9F1FF] hover:cursor-pointer hover:bg-blue-100 border-solid border-[1px] border-transparent hover:border-solid hover:border-[1px] hover:border-blue-300 transition-all ease-out"
+            className="flex justify-center items-center gap-4 py-3 w-full rounded-md bg-[#E9F1FF] hover:cursor-pointer hover:bg-blue-100 border-solid border-[1px] border-transparent hover:border-blue-300 transition-all ease-out"
           >
             <Image
               src={"/assets/logos/microsoft.svg"}
-              width={200}
-              height={200}
+              width={20}
+              height={20}
               alt="Microsoft Logo"
-              className="size-5"
             />
             <span className="text-[#4285F4]">Iniciar con Microsoft</span>
           </button>
 
-          <form className="flex flex-col gap-4 pt-5">
-            {/* Email */}
+          {/* Login con email */}
+          <form
+            onSubmit={handleLoginWithEmail}
+            className="flex flex-col gap-4 pt-5"
+          >
             <div className="flex flex-col gap-2">
               <label htmlFor="email" className="pl-1">
                 Correo electrónico
               </label>
               <input
-                type="text"
+                type="email"
                 id="email"
                 name="email"
-                placeholder="ejemplo@hotmail.com"
+                placeholder="ejemplo@empresa.com"
                 className="p-2 pl-4 text-[14px] bg-neutral-100 border-[1px] border-neutral-200 rounded-md transition-all focus:border-blue-700 focus:outline-0"
                 required
               />
             </div>
 
-            {/* Password */}
             <div className="flex flex-col gap-2">
               <label htmlFor="password" className="pl-1">
                 Contraseña
@@ -117,45 +156,34 @@ export default function Home() {
                   required
                 />
 
-                <div className="absolute flex justify-end items-center w-full h-10 -translate-y-10 pointer-events-none">
-                  <div
-                    onClick={() => {
-                      setShowPassword(!showPassword);
-                    }}
-                    className={`size-6 absolute mr-4 hover:cursor-pointer pointer-events-auto`}
-                  >
-                    {showPassword ? (
-                      <>
-                        <Image
-                          src={"/assets/icons/eye-closed.svg"}
-                          layout="fill"
-                          alt="Eye hide password"
-                          className="opacity-60"
-                        />
-                      </>
-                    ) : (
-                      <>
-                        <Image
-                          src={"/assets/icons/eye.svg"}
-                          layout="fill"
-                          objectFit="cover"
-                          alt="Eye password"
-                          className="opacity-60"
-                        />
-                      </>
-                    )}
-                  </div>
+                <div
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                >
+                  <Image
+                    src={
+                      showPassword
+                        ? "/assets/icons/eye-closed.svg"
+                        : "/assets/icons/eye.svg"
+                    }
+                    width={20}
+                    height={20}
+                    alt="Toggle password visibility"
+                    className="opacity-60"
+                  />
                 </div>
               </div>
             </div>
 
-            {/* Submit Button */}
             {error && <p className="text-sm text-red-500">{error}</p>}
             <button
               type="submit"
-              className={`mt-4 bg-blue-500 text-white text-[18px] rounded-xl py-3 shadow-md hover:cursor-pointer`}
+              disabled={isLoading}
+              className={`mt-4 bg-blue-500 text-white text-[18px] rounded-xl py-3 shadow-md hover:cursor-pointer ${
+                isLoading ? "opacity-70 cursor-not-allowed" : ""
+              }`}
             >
-              Iniciar Sesión
+              {isLoading ? "Iniciando..." : "Iniciar Sesión"}
             </button>
           </form>
         </div>
